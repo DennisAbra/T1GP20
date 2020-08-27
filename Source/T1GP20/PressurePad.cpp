a9d3821e5c912509eb7687968bc0c5f484c5a2fe
Mandy_Fu <man.chunfu@futuregames.nu>
2020-08-27 16:53:43 +0200

    update pressurepad

--- Source/T1GP20/PressurePad.cpp
+++ Source/T1GP20/PressurePad.cpp
@@ -38,7 +38,7 @@ void APressurePad::BeginPlay()
 
 void APressurePad::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (TriggerDoor)
+	if (TriggerDoor && OtherActor)
 	{
 		AItem* Object = Cast<AItem>(OtherActor);
 		if (Object)
