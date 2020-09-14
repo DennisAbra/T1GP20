101ca69779bc2e78e0a97977ea109d4b8e039c20
5piffe <spiffe84@gmail.com>
2020-09-14 15:19:35 +0200

    Anchor thud

--- Source/T1GP20/Item.h
+++ Source/T1GP20/Item.h
@@ -47,7 +47,7 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item | Other")
 	bool bOffCenterHoldingPos;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item | Sound", meta = (ToolTip = "0 = Default\n1 = Wood\n2 = Glass\n3 = GlassBreak\n 4 = Rock\n5 = Heavy Metal\n6 = Bone\n"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item | Sound", meta = (ToolTip = "0 = Default\n1 = Wood\n2 = Glass\n3 = GlassBreak\n 4 = Rock\n5 = Heavy Metal\n6 = Bone\n7 = Anchor(small thing)"))
 	int MaterialInt = 0;
 
 protected:
